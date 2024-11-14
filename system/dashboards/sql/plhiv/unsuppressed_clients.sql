2a0bac0e1e3db118c85f7d91f58557d218a41144
bernard-siele <bernard.siele@ucglobalprograms.org>
2024-11-14 16:12:40 +0300

    Added date initiated EAC to unsuppressed clients

--- system/dashboards/sql/plhiv/unsuppressed_clients.sql
+++ system/dashboards/sql/plhiv/unsuppressed_clients.sql
@@ -9,6 +9,16 @@ WITH last_vl AS (
         ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY viral_load_result_date DESC) AS rn
     FROM
         derived.fact_viral_load_test
+),
+next_eac AS (
+    SELECT
+        client_id,
+        date_initiated_eac,
+        date_completed_eac,
+        date_hvl_test_after_eac,
+        ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY date_initiated_eac ASC) AS rn
+    FROM
+        derived.fact_eac
 )
 
 SELECT
@@ -20,11 +30,18 @@ SELECT
     lv.viral_load_test_date AS [Last VL Test Date],
     lv.viral_load_result_date AS [Last VL Result Date],
     lv.viral_load_test_result_numeric AS [Last VL Result],
-    lv.is_viral_load_suppressed_50 AS [Last VL Suppressed]
+    CASE WHEN lv.viral_load_result_date IS NOT NULL AND lv.is_viral_load_suppressed_50 = 0 THEN 'N' END AS [Last VL Suppressed?],
+    ne.date_initiated_eac AS [Date Initiated EAC]
 FROM
     derived.fact_ctc_daily_client_status f
 INNER JOIN
     last_vl lv ON f.client_id = lv.client_id
+LEFT JOIN
+    next_eac ne ON f.client_id = ne.client_id
+AND
+    ne.rn = 1
+AND
+    lv.viral_load_result_date < ne.date_initiated_eac
 INNER JOIN
     derived.dim_date d ON d.[date] = lv.viral_load_test_date
 WHERE
@@ -32,8 +49,8 @@ WHERE
 AND
     lv.rn = 1
 AND
-    lv.viral_load_result_date < f.report_date
+    lv.viral_load_test_date < f.report_date
 AND
-    d.weekly_start_monday_end_date = COALESCE(NULLIF('$week', ''), '2024-06-02')
+    d.weekly_start_monday_end_date = COALESCE(NULLIF('$week', ''), '2024-06-09')
 ORDER BY
     f.report_date ASC
