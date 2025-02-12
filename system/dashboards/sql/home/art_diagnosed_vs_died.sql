1bcf88bc3a3be7a56aedd1a3c18d50b7dce7dd3a
Davidson Gikandi <ngibuinigikandi@gmail.com>
2025-02-12 16:29:34 +0300

    - Enhanced waterfall chart

--- system/dashboards/sql/home/art_diagnosed_vs_died.sql
+++ system/dashboards/sql/home/art_diagnosed_vs_died.sql
@@ -1,4 +1,7 @@
-select month_name AS category , sum(value)  as value
+select category as label, sum(value) as value
+from 
+(
+select SUBSTR(month_name, 1, 3) AS category, month_of_year , sum(value)  as value
 from 
 (
 SELECT
@@ -36,9 +39,9 @@ FROM
 		INNER JOIN dim_hiv_diagnosis_facility f ON f.facility_id = s.hiv_diagnosis_facility_id
 WHERE exit_reason = 'Died'
 GROUP BY gender, ten_year_interval, year, month_name, month_of_year
-
-
 ) a
 where (gender = '$gender' OR '$gender' = '') AND (age_group = '$age_group' OR '$age_group' = '') AND (year = COALESCE(NULLIF('$year', ''), '2021'))
-GROUP BY category_month
-ORDER BY CAST(month_of_year AS integer)
+group by category_month, month_of_year
+) a
+group by category
+order by CAST(month_of_year AS integer)
