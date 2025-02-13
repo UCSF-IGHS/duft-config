c98f3da52521ba79f95b34ce43303e2fec13dfd1
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-02-13 15:30:19 +0300

    Used DataProvider and QueryData

--- system/dashboards/sql/table_list.sql
+++ system/dashboards/sql/table_list.sql
@@ -1,6 +1,7 @@
 SELECT
-    s.name AS schema_name,
-    t.name AS table_name
+    s.name AS [Schema Name],
+    t.name AS [Table Name],
+    CONCAT(s.name, '.', t.name) AS [Copy For Database]
 FROM ctc_a_prod_analysis_test.sys.tables t
 INNER JOIN ctc_a_prod_analysis_test.sys.schemas s ON t.schema_id = s.schema_id
--- ORDER BY s.name, t.name
+ORDER BY s.name
