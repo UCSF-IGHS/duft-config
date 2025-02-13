bf3c237cf15def9759fd00f48fdedc906df82f5d
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-02-13 10:22:55 +0300

    Added the table list view

--- system/dashboards/sql/table_list.sql
+++ system/dashboards/sql/table_list.sql
@@ -1,6 +1,6 @@
 SELECT
     s.name AS schema_name,
     t.name AS table_name
-FROM sys.tables t
-INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
-ORDER BY s.name, t.name
+FROM ctc_a_prod_analysis_test.sys.tables t
+INNER JOIN ctc_a_prod_analysis_test.sys.schemas s ON t.schema_id = s.schema_id
+-- ORDER BY s.name, t.name
