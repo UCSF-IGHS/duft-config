4ae1dc3c7fc2ce22971677e7b99acbe90ce373e5
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-02-14 10:29:20 +0300

    Updated the not_registered sql query

--- system/dashboards/sql/hei/not_registered.sql
+++ system/dashboards/sql/hei/not_registered.sql
@@ -1,15 +1,19 @@
-WITH first_visit AS (
+WITH first_visit_after_delivery AS (
     SELECT
-        client_id,
-        visit_date,
-        ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY visit_date ASC) AS rn
-    FROM derived.fact_visit
+        fv.client_id,
+        fv.visit_date,
+        ROW_NUMBER() OVER (PARTITION BY fv.client_id ORDER BY fv.visit_date ASC) AS rn
+    FROM
+        derived.fact_visit fv
+    INNER JOIN
+        base.fact_pregnancy fp
+        ON fv.client_id = fp.ctc_client_id
+    WHERE fv.visit_date > fp.delivery_date
 )
 
 SELECT
     dc.client_id AS [Client ID],
     expected_delivery_date AS [Due Date],
-    expected_delivery_date AS [EDD],
     delivery_date AS [Delivery Date],
     gender AS [Sex],
     pregnancy_outcome AS [Pregnancy Outcome],
@@ -17,16 +21,21 @@ SELECT
     CASE
         WHEN date_of_death IS NULL
             THEN dbo.fn_staging_calculate_age(date_of_birth, GETDATE())
+        ELSE dbo.fn_staging_calculate_age(date_of_birth, date_of_death)
     END AS [Current Age],
-    visit_date AS [Registration Date],
+    fv.visit_date AS [Registration Date],
     date_of_death AS [Date of Death]
-FROM base.fact_pregnancy fp
+FROM
+    base.fact_pregnancy fp
 INNER JOIN
     derived.dim_client dc
     ON fp.ctc_client_id = dc.client_id
-INNER JOIN
-    first_visit fv
+LEFT JOIN
+    first_visit_after_delivery fv
     ON fp.ctc_client_id = fv.client_id
     AND fv.rn = 1
+WHERE
+    fp.delivery_date IS NOT NULL
+    AND fv.visit_date IS NULL
 ORDER BY
-    dc.client_id
+    ctc_client_id
