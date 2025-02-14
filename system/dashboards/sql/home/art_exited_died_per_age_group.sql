f19a44417217589a94e32c1d551eb3b7b7e6dca6
Davidson Gikandi <ngibuinigikandi@gmail.com>
2025-02-14 14:42:19 +0300

    - Enhancements

--- system/dashboards/sql/home/art_exited_died_per_age_group.sql
+++ system/dashboards/sql/home/art_exited_died_per_age_group.sql
@@ -1,7 +1,13 @@
 SELECT
 	TRIM( year ) AS category,
-	SUM( CASE WHEN gender = 'Male' THEN 1 ELSE 0 END ) AS Male,
-	SUM( CASE WHEN gender = 'Female' THEN 1 ELSE 0 END ) AS Female 
+	SUM( CASE WHEN TRIM(ag.ten_year_interval) = '0-9' THEN 1 ELSE 0 END ) AS '0-9',
+	SUM( CASE WHEN TRIM(ag.ten_year_interval) = '10-19' THEN 1 ELSE 0 END ) AS '10-19' ,
+	SUM( CASE WHEN TRIM(ag.ten_year_interval) = '20-29' THEN 1 ELSE 0 END ) AS '20-29',
+	SUM( CASE WHEN TRIM(ag.ten_year_interval) = '30-39' THEN 1 ELSE 0 END ) AS '30-39' ,
+	SUM( CASE WHEN TRIM(ag.ten_year_interval) = '40-49' THEN 1 ELSE 0 END ) AS '40-49',
+	SUM( CASE WHEN TRIM(ag.ten_year_interval) = '50-59' THEN 1 ELSE 0 END ) AS '50-59' ,
+	SUM( CASE WHEN TRIM(ag.ten_year_interval) = '60-69' THEN 1 ELSE 0 END ) AS '60-69',
+	SUM( CASE WHEN TRIM(ag.ten_year_interval) = '70+' THEN 1 ELSE 0 END ) AS '70+' 
 FROM
 	fact_sentinel_event se
 	INNER JOIN dim_client c ON se.client_id = c.client_id
