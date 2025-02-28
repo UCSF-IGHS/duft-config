53868b7adca3dd67a7374278dfb4301b9ef0817d
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-02-28 12:32:45 +0300

    Worked on HEI not registered

--- system/dashboards/sql/hei/need_eid_test.sql
+++ system/dashboards/sql/hei/need_eid_test.sql
@@ -1,55 +1,17 @@
-WITH first_eid_test AS (
-    SELECT
-        fet.exposed_infant_id,
-        fet.mother_ctc_number,
-        fet.exposed_infant_number,
-        fet.eid_test_date,
-        fet.eid_test_type,
-        fet.eid_result_date,
-        fet.eid_result,
-        ROW_NUMBER() OVER (PARTITION BY fet.exposed_infant_number ORDER BY fet.eid_test_date ASC) AS rn
-    FROM
-         base.fact_eid_test fet
-    WHERE
-        eid_test_type = 'D'
-), last_eid_visit AS (
-    SELECT
-        lev.exposed_infant_id,
-        lev.eid_visit_date,
-        ROW_NUMBER() OVER (PARTITION BY lev.exposed_infant_id ORDER BY lev.eid_visit_date DESC) AS rn
-    FROM
-        base.fact_eid_visits lev
-)
-
 SELECT
-    dc.ctc_id AS [CTC ID],
-    dei.exposed_infant_number AS [Exposed Infant Number],
-    gender AS [Sex],
-    exposed_infant_date_of_birth AS [DOB],
-    dbo.fn_calculate_months_between_dates(exposed_infant_date_of_birth, GETDATE()) AS [Age in Months],
-    lev.eid_visit_date AS [Last EID Visit Date],
-    CASE
-        WHEN fet.eid_test_date IS NULL
-            THEN 'Yes'
-        ELSE 'No'
-    END AS [Eligible for EID?],
-    fet.eid_test_date AS [First EID Test Date],
-    fet.eid_test_type AS [First EID Test Type],
-    fet.eid_result_date AS [First EID Result Date],
-    fet.eid_result AS [First EID Result]
+    [Mother CTC ID],
+    [Exposed Infant Number],
+    [Child CTC ID],
+    [Sex],
+    [DOB],
+    [Age in Months],
+    [Last EID Visit Date],
+    [Eligible for First EID Test?]
 FROM
-    derived.dim_exposed_infant dei
-INNER JOIN derived.dim_client dc
-    ON dei.mother_ctc_number = dc.ctc_id
-LEFT JOIN
-    first_eid_test fet
-    ON dei.exposed_infant_id = fet.exposed_infant_id
-    AND fet.rn = 1
-LEFT JOIN
-    last_eid_visit lev
-    ON dei.exposed_infant_id = lev.exposed_infant_id
-    AND lev.rn = 1
+    duft.fact_duft_hei_sentinel_event
 WHERE
-    fet.eid_test_date IS NULL
+    [Eligible for First EID Test?] = 'Yes'
+AND
+    ISNULL([Infant Status], '') NOT IN ('TRN', 'DIE')
 ORDER BY
-    dc.client_id
+    [Mother CTC ID]
--- system/dashboards/sql/hei/not_registered.sql
+++ system/dashboards/sql/hei/not_registered.sql
@@ -1,42 +1,20 @@
-WITH first_visit_after_delivery AS (
-    SELECT
-        fv.client_id,
-        fv.visit_date,
-        ROW_NUMBER() OVER (PARTITION BY fv.client_id ORDER BY fv.visit_date ASC) AS rn
-    FROM
-        derived.fact_visit fv
-    INNER JOIN
-        base.fact_pregnancy fp
-        ON fv.client_id = fp.ctc_client_id
-    WHERE fv.visit_date > fp.delivery_date
-)
-
 SELECT
-    dc.ctc_id AS [CTC ID],
-    dc.client_id AS [Client ID],
-    expected_delivery_date AS [Due Date],
-    delivery_date AS [Delivery Date],
-    gender AS [Sex],
-    pregnancy_outcome AS [Pregnancy Outcome],
-    dei.exposed_infant_date_of_birth AS [DOB],
-    dbo.fn_calculate_months_between_dates(dei.exposed_infant_date_of_birth, GETDATE()) AS [Age in Months],
-    fv.visit_date AS [Registration Date],
-    date_of_death AS [Date of Death]
+    [Mother CTC ID],
+    [Exposed Infant Number],
+    [Child CTC ID],
+    [Sex],
+    [DOB],
+    [Age in Months],
+    [Due Date],
+    [Delivery Date],
+    [First Visit Date After Delivery],
+    [HEI Registered?],
+    [Infant Status]
 FROM
-    base.fact_pregnancy fp
-INNER JOIN
-    derived.dim_client dc
-    ON fp.ctc_client_id = dc.client_id
-INNER JOIN derived.dim_exposed_infant dei
-    ON dei.mother_ctc_number = dc.ctc_id
-INNER JOIN ctc_a_source.dbo.tblExposedInfants tei
-    ON dei.exposed_infant_number = tei.ExposedInfantID
-LEFT JOIN
-    first_visit_after_delivery fv
-    ON fp.ctc_client_id = fv.client_id
-    AND fv.rn = 1
+    duft.fact_duft_hei_sentinel_event
 WHERE
-    fp.delivery_date IS NOT NULL
-    AND fv.visit_date IS NULL
+    [HEI Registered?] = 'No'
+AND
+    ISNULL([Infant Status], '') NOT IN ('TRN', 'DIE')
 ORDER BY
-    ctc_client_id
+    [Mother CTC ID]
