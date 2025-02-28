b718d5c55d0c0107d0ad961b7c97d20dab216563
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-02-28 13:03:26 +0300

    Modified the query for HIE without final outcome

--- system/dashboards/sql/hei/without_final_outcome.sql
+++ system/dashboards/sql/hei/without_final_outcome.sql
@@ -1,20 +1,18 @@
 SELECT
-    dc.ctc_id AS [CTC ID],
-    dei.exposed_infant_number AS [Exposed Infant Number],
-    gender AS [Sex],
-    exposed_infant_date_of_birth AS [DOB],
-    dbo.fn_staging_calculate_age(exposed_infant_date_of_birth, GETDATE()) AS [Current Age],
-    dbo.fn_calculate_months_between_dates(exposed_infant_date_of_birth, GETDATE()) AS [Age in Months]
-    -- feic.exposed_infant_outcome AS [Final Outcome],
-    -- feic.exposed_infant_outcome_date AS [Final Outcome Date]
+    [Mother CTC ID],
+    [Exposed Infant Number],
+    [Child CTC ID],
+    [Sex],
+    [DOB],
+    [Age in Months],
+    [Has Final Outcome?]
 FROM
-    derived.dim_exposed_infant dei
-INNER JOIN derived.dim_client dc
-    ON dei.mother_ctc_number = dc.ctc_id
-LEFT JOIN base.fact_exposed_infant_outcome feic
-    ON dei.exposed_infant_id = feic.exposed_infant_id
+    duft.fact_duft_hei_sentinel_event
 WHERE
-    dbo.fn_calculate_months_between_dates(exposed_infant_date_of_birth, GETDATE()) BETWEEN 18 AND 24
-    AND feic.exposed_infant_outcome IS NULL
+    [Has Final Outcome?] = 'No'
+AND
+    [Age in Months] BETWEEN 18 AND 24
+AND
+    ISNULL([Infant Status], '') NOT IN ('TRN', 'DIE')
 ORDER BY
-    dc.client_id
+    [Mother CTC ID]
