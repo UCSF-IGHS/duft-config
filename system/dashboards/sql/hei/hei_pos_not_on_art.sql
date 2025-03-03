e24ac888bf6f736dcbae158906982e4b13104d1d
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-03-03 16:58:39 +0300

    Modified HEI POS not on ART script

--- system/dashboards/sql/hei/hei_pos_not_on_art.sql
+++ system/dashboards/sql/hei/hei_pos_not_on_art.sql
@@ -1,58 +1,19 @@
-WITH last_antibody_test AS (
-    SELECT
-        lat.exposed_infant_id,
-        lat.mother_ctc_number,
-        lat.exposed_infant_number,
-        lat.eid_test_date,
-        lat.eid_test_type,
-        lat.eid_result_date,
-        lat.eid_result,
-        ROW_NUMBER() OVER (PARTITION BY lat.exposed_infant_number ORDER BY lat.eid_test_date DESC) AS rn
-    FROM
-         base.fact_eid_test lat
-    WHERE
-        eid_test_type = 'A'
-), last_eid_visit AS (
-    SELECT
-        lev.exposed_infant_id,
-        lev.eid_visit_date,
-        ROW_NUMBER() OVER (PARTITION BY lev.exposed_infant_id ORDER BY lev.eid_visit_date DESC) AS rn
-    FROM
-        base.fact_eid_visits lev
-)
-
 SELECT
-    dc.ctc_id AS [CTC ID],
-    dei.exposed_infant_number AS [Exposed Infant Number],
-    gender AS [Sex],
-    exposed_infant_date_of_birth AS [DOB],
-    dbo.fn_calculate_months_between_dates(exposed_infant_date_of_birth, GETDATE()) AS [Age in Months],
-    lev.eid_visit_date AS [Last EID Visit Date],
-    lat.eid_test_date AS [Last Antibody Test Date],
-    lat.eid_test_type AS [Last Antibody Test Type],
-    lat.eid_result_date AS [Last Antibody Result Date],
-    lat.eid_result AS [Last Antibody Result],
-    tei.ChildPatientID AS [Child's CTC ID],
-    CASE
-        WHEN tei.ChildPatientID IS NULL OR TRIM(tei.ChildPatientID) = ''
-            THEN 'No'
-        ELSE 'Yes'
-    END AS [Initiated on ART?]
+    [Mother CTC ID],
+    [Exposed Infant Number],
+    [Child CTC ID],
+    [Sex],
+    [DOB],
+    [Age in Months],
+    [Last EID Visit Date],
+    [Last Antibody Test Date],
+    [Last Antibody Test Type],
+    [Last Antibody Result Date],
+    [Last Antibody Result],
+    [Initiated on ART?]
 FROM
-    derived.dim_exposed_infant dei
-INNER JOIN derived.dim_client dc
-    ON dei.mother_ctc_number = dc.ctc_id
-INNER JOIN ctc_a_source.dbo.tblExposedInfants tei
-    ON dei.exposed_infant_number = tei.ExposedInfantID
-LEFT JOIN
-    last_antibody_test lat
-    ON dei.exposed_infant_id = lat.exposed_infant_id
-    AND lat.rn = 1
-LEFT JOIN
-    last_eid_visit lev
-    ON dei.exposed_infant_id = lev.exposed_infant_id
-    AND lev.rn = 1
+    duft.fact_duft_hei_sentinel_event
 WHERE
-    lat.eid_result = 'POS'
+    [Last Antibody Result] = 'POS'
 ORDER BY
-    dc.client_id
+    [Mother CTC ID]
