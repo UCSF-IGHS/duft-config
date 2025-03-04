433d4243ea2b105d4d193ca23df863ca3eafaa1b
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-03-04 18:43:30 +0300

    Added and removed columns for AHD clients list as required

--- system/dashboards/sql/plhiv/ahd_clients.sql
+++ system/dashboards/sql/plhiv/ahd_clients.sql
@@ -2,9 +2,12 @@ SELECT
     [CTC ID],
     [Current Age],
     [Sex],
+    [Last Visit Date],
+    [Last CD4 Test Date],
     [Last CD4 Result Date],
     [Last CD4 Result Count],
     [Last CD4 < 200],
+    [WHO Stage 3/4 Result],
     [WHO Stage 3/4 With No CD4 Test],
     [AHD Suspect]
 FROM
