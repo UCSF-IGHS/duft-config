c78713442a49934a0131b74ed3f8e3b8f81c923c
Davidson Gikandi <ngibuinigikandi@gmail.com>
2025-03-10 11:56:03 +0300

    - Cleaned up wakanda visuals

--- system/dashboards/sql/clients/art_regimen_by_age_group_wk.sql
+++ system/dashboards/sql/clients/art_regimen_by_age_group_wk.sql
@@ -1,10 +1,14 @@
 SELECT 
-    a.ag AS category,
-    COALESCE(SUM(CASE WHEN v.category = 'First line' THEN v.value ELSE 0 END), 0) AS 'First line',
-    COALESCE(SUM(CASE WHEN v.category = 'Second line' THEN v.value ELSE 0 END), 0) AS 'Second line',
-    COALESCE(SUM(CASE WHEN v.category = 'Third line' THEN v.value ELSE 0 END), 0) AS 'Third line',
-    COALESCE(SUM(CASE WHEN v.category = 'Unknown line' THEN v.value ELSE 0 END), 0) AS 'Unknown line'
+    v.category,
+    SUM(CASE WHEN a.ag = '0-9' THEN value ELSE 0 END) AS '0-9',
+    SUM(CASE WHEN a.ag = '10-19' THEN value ELSE 0 END) AS '10-19', 
+    SUM(CASE WHEN a.ag = '20-29' THEN value ELSE 0 END) AS '20-29', 
+    SUM(CASE WHEN a.ag = '30-39' THEN value ELSE 0 END) AS '30-39',
+    SUM(CASE WHEN a.ag = '40-49' THEN value ELSE 0 END) AS '40-49', 
+    SUM(CASE WHEN a.ag = '50-59' THEN value ELSE 0 END) AS '50-59', 
+    SUM(CASE WHEN a.ag = '60-69' THEN value ELSE 0 END) AS '60-69', 
+    SUM(CASE WHEN a.ag = '70+' THEN value ELSE 0 END) AS '70+'
 FROM (SELECT DISTINCT TRIM(ten_year_interval) as ag FROM dim_age_group) a
 LEFT JOIN vw_art_regimen_lines v 
     ON a.ag = v.age_group  
-GROUP BY a.ag
+GROUP BY category
