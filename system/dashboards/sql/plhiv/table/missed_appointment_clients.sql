289d66074d942c8b051675137ac5bccb7c980e51
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-03-18 15:47:19 +0300

    Update days prescribed and last appointment documented status

--- system/dashboards/sql/plhiv/table/missed_appointment_clients.sql
+++ system/dashboards/sql/plhiv/table/missed_appointment_clients.sql
@@ -4,11 +4,11 @@ SELECT
     [Sex],
     [Last Visit Date],
     [Last Appointment Date] AS [Missed Appointment Date],
-    --Days Prescribed
-    --Last Documented
+    [Number of Days Prescribed],
+    [Last Appointment Documented Status],
     [Now Pregnant/Breastfeeding],
-    -- [TB Suspect]
-    [Has High VL Post EAC] --!?
+    NULL AS [TB Suspect],
+    [First VL Is Unsuppressed] AS [Has High VL Post EAC]
 FROM
     duft.fact_duft_sentinel_event
 WHERE
