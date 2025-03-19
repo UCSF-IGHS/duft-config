53019ede7f2ef8301e3a31e313da05bdb998db94
Davidson Gikandi <ngibuinigikandi@gmail.com>
2025-03-19 14:42:32 +0300

    - Applied filters

--- system/dashboards/sql/visual/vls_overview.sql
+++ system/dashboards/sql/visual/vls_overview.sql
@@ -1,1 +1,43 @@
-SELECT categories.DATA AS category, COUNT(a.DATA) AS value, CASE WHEN (SELECT COUNT(*) FROM analysis.fact_sentinel_event WHERE "__client_id" not in ( select "__client_id" from analysis.fact_sentinel_event where "TX Curr"='Yes' AND ("Months Since ART Start" < 6 or "Months Since ART Restart" < 6)) AND "Last Viral Load Result Category" IS NOT NULL AND "Viral Load Eligibility" IN ('Monitored per Guidelines',  'Slightly Delayed')) = 0 THEN '0%' ELSE CONCAT(ROUND((COUNT(a.DATA) * 100.0 / (SELECT COUNT(*) FROM analysis.fact_sentinel_event WHERE "__client_id" not in ( select "__client_id" from analysis.fact_sentinel_event where "TX Curr"='Yes' AND ("Months Since ART Start" < 6 or "Months Since ART Restart" < 6)) AND "Last Viral Load Result Category" IS NOT NULL AND "Viral Load Eligibility" IN ('Monitored per Guidelines',  'Slightly Delayed')   )), 1), '%') END AS percentage FROM (SELECT 'Undetectable' AS DATA UNION ALL SELECT 'LLV' UNION ALL SELECT 'Unsuppressed') AS categories LEFT JOIN (SELECT "Tri Pillar Age Group", "Last Viral Load Result Category" AS DATA, "Sex", "Last Care Model" FROM analysis.fact_sentinel_event WHERE "__client_id" not in ( select "__client_id" from analysis.fact_sentinel_event where "TX Curr"='Yes' AND ("Months Since ART Start" < 6 or "Months Since ART Restart" < 6)) AND "Last Viral Load Result Category" IS NOT NULL AND "Viral Load Eligibility" IN ('Monitored per Guidelines',  'Slightly Delayed')  ) a ON categories.DATA = a.DATA GROUP BY categories.DATA ORDER BY CASE WHEN categories.DATA = 'Undetectable' THEN 1 WHEN categories.DATA = 'LLV' THEN 2 WHEN categories.DATA = 'Unsuppressed' THEN 3 END
+WITH total_count AS (
+    SELECT 
+        SUM(CASE 
+            WHEN fse."__client_id" NOT IN (
+                SELECT "__client_id" 
+                FROM analysis.active_clients 
+            ) 
+            AND fse."Last Viral Load Result Category" IS NOT NULL 
+            AND fse."Viral Load Eligibility" IN ('Monitored per Guidelines', 'Slightly Delayed') 
+        THEN 1 ELSE 0 
+        END) AS total
+    FROM analysis.fact_sentinel_event fse
+)
+SELECT 
+    categories.DATA AS category, 
+    COUNT(a."Last Viral Load Result Category") AS value, 
+    CASE 
+        WHEN total.total = 0 THEN '0%' 
+        ELSE CONCAT(
+            ROUND((COUNT(a."Last Viral Load Result Category") * 100.0 / NULLIF(total.total, 0)), 1), '%'
+        ) 
+    END AS percentage
+FROM 
+    (VALUES ('Undetectable'), ('LLV'), ('Unsuppressed')) AS categories(DATA)
+LEFT JOIN 
+    analysis.fact_sentinel_event a 
+    ON categories.DATA = a."Last Viral Load Result Category"
+    AND a."__client_id" NOT IN (
+        SELECT "__client_id" 
+				FROM analysis.active_clients 
+    )
+    AND a."Last Viral Load Result Category" IS NOT NULL
+    AND a."Viral Load Eligibility" IN ('Monitored per Guidelines', 'Slightly Delayed')
+CROSS JOIN 
+    total_count total
+GROUP BY 
+    categories.DATA, total.total
+ORDER BY 
+    CASE 
+        WHEN categories.DATA = 'Undetectable' THEN 1 
+        WHEN categories.DATA = 'LLV' THEN 2 
+        WHEN categories.DATA = 'Unsuppressed' THEN 3 
+    END
