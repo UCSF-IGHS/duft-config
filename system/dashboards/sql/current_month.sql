231c394c0680963191a64c4c0a38d4532968fae6
TrevorAntony <trvromondi@gmail.com>
2025-03-25 13:03:44 +0300

    added current_month to reference 3DL

---
+++ system/dashboards/sql/current_month.sql
@@ -0,0 +1,1 @@
+SELECT CASE strftime('%m', 'now') WHEN '01' THEN 'January' WHEN '02' THEN 'February' WHEN '03' THEN 'March' WHEN '04' THEN 'April' WHEN '05' THEN 'May' WHEN '06' THEN 'June' WHEN '07' THEN 'July' WHEN '08' THEN 'August' WHEN '09' THEN 'September' WHEN '10' THEN 'October' WHEN '11' THEN 'November' WHEN '12' THEN 'December' END AS current_month
