2ca06c578e03e2e11c14279d53c84c3aacb5b5f3
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-03-27 12:45:31 +0300

    Chore: Use correct filter for the CLHIV with nutrition issues

--- system/dashboards/sql/plhiv/table/clhiv_nutrition_clients.sql
+++ system/dashboards/sql/plhiv/table/clhiv_nutrition_clients.sql
@@ -15,5 +15,15 @@ WHERE
     [Current Age] < 15
 AND
     [Last Visit in Previous Week] = 'Yes'
+AND
+(
+    [Current Weight] IS NULL
+OR
+    [Current Weight] IS NULL
+OR
+    [Documented Nutritional Status] IS NULL
+OR
+    [Documented and Calculated Nutritional Status Match] = 'No'
+)
 ORDER BY
     [Patient ID] ASC
--- system/dashboards/sql/plhiv/tile/clhiv_nutrition.sql
+++ system/dashboards/sql/plhiv/tile/clhiv_nutrition.sql
@@ -6,3 +6,13 @@ WHERE
     [Current Age] < 15
 AND
     [Last Visit in Previous Week] = 'Yes'
+AND
+(
+    [Current Weight] IS NULL
+OR
+    [Current Weight] IS NULL
+OR
+    [Documented Nutritional Status] IS NULL
+OR
+    [Documented and Calculated Nutritional Status Match] = 'No'
+)
