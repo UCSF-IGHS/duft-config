2076cb61e30be6173a0803f9801f7fe80abf84eb
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-04-09 09:55:48 +0300

    Chore: Update prescribed column to dispensed

--- system/dashboards/sql/plhiv/table/missed_appointment_clients.sql
+++ system/dashboards/sql/plhiv/table/missed_appointment_clients.sql
@@ -5,8 +5,8 @@ SELECT
     [Date Start ART],
     [Last Visit Date],
     [Last Appointment Date] AS [Missed Appointment Date],
-    [Number of Days Prescribed],
-    [Last Appointment Documented Status],
+    [Number of Days Dispensed],
+    [Last Documented Appointment Status],
     [Now Pregnant/Breastfeeding],
     NULL AS [TB Suspect],
     [Last VL Is Unsuppressed] AS [High VL]
