e93215ab93c2f8a066d34b0fc163b40b1b09d4b7
Tumshukuru Mhalila <tmhalila@gmail.com>
2025-04-11 13:22:50 +0300

    Chore: Add VL results CD4 in last week for PLHIV

--- system/dashboards/sql/plhiv/tile/unsuppressed.sql
+++ system/dashboards/sql/plhiv/tile/unsuppressed.sql
@@ -3,12 +3,11 @@ SELECT
 FROM
     duft.fact_duft_sentinel_event
 WHERE
-   ( 
-    [Last Appointment in Previous Week] = 'Yes'
-AND
-    [Last VL Is Unsuppressed] = 'Yes'
-   )
-OR
-    [Last VL Result in Previous Week] = 'Yes'
-AND
-    [Last Date Initiated EAC] IS NULL
+    (
+        [Last VL Is Unsuppressed] = 'Yes'
+        AND [Last Date Initiated EAC] IS NULL
+    )
+    AND (
+        [Last Appointment in Previous Week] = 'Yes'
+        OR [Last VL Result in Previous Week] = 'Yes'
+    )
