fcba9353712cf9ad213b7d3818d09106a7a1a8c0
Tumshukuru Mhalila <tmhalila@gmail.com>
2025-04-11 12:50:52 +0300

    Chore: Update the PLHIV to include results of previous week

--- system/dashboards/sql/plhiv/tile/unsuppressed.sql
+++ system/dashboards/sql/plhiv/tile/unsuppressed.sql
@@ -3,8 +3,12 @@ SELECT
 FROM
     duft.fact_duft_sentinel_event
 WHERE
+   ( 
     [Last Appointment in Previous Week] = 'Yes'
 AND
     [Last VL Is Unsuppressed] = 'Yes'
+   )
+OR
+    [Last VL Result in Previous Week] = 'Yes'
 AND
     [Last Date Initiated EAC] IS NULL
