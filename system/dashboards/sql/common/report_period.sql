f7e58db5f3beead40cde3c2c1095992506fcac96
Tumshukuru Mhalila <tmhalila@gmail.com>
2025-04-11 12:44:07 +0300

    Chore: Update report period label

--- system/dashboards/sql/common/report_period.sql
+++ system/dashboards/sql/common/report_period.sql
@@ -1,1 +1,1 @@
- SELECT CONCAT('previous week (', previous_week_dates, ')') AS report_week_dates FROM duft.dim_reporting_week;
+ SELECT CONCAT('Previous Week (', previous_week_dates, ')') AS report_week_dates FROM duft.dim_reporting_week;
