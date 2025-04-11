bafd41e7777636aa34d6f47820800a8c69a90fb8
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-04-12 01:19:17 +0300

    Chore: Minor formatting

--- system/dashboards/sql/plhiv/table/clhiv_nutrition_clients.sql
+++ system/dashboards/sql/plhiv/table/clhiv_nutrition_clients.sql
@@ -14,17 +14,14 @@ FROM
     duft.fact_duft_sentinel_event
 WHERE
     [Current Age] < 5
-AND
-    [Last Visit in Previous Week] = 'Yes'
-AND
-(
-    [Current Weight(KG)] IS NULL
-OR
-    [Current Height(KG)] IS NULL
-OR
-    [Documented Nutritional Status] IS NULL
-OR
-    [Documented and Calculated Nutritional Status Match] = 'No'
-)
+    AND
+        [Last Visit in Previous Week] = 'Yes'
+    AND
+    (
+        [Current Weight(KG)] IS NULL
+        OR [Current Height(CM)] IS NULL
+        OR [Documented Nutritional Status] IS NULL
+        OR [Documented and Calculated Nutritional Status Match] = 'No'
+    )
 ORDER BY
     [Patient ID] ASC
--- system/dashboards/sql/plhiv/tile/clhiv_nutrition.sql
+++ system/dashboards/sql/plhiv/tile/clhiv_nutrition.sql
@@ -4,15 +4,12 @@ FROM
     duft.fact_duft_sentinel_event
 WHERE
     [Current Age] < 5
-AND
-    [Last Visit in Previous Week] = 'Yes'
-AND
-(
-    [Current Weight(KG)] IS NULL
-OR
-    [Current Weight(KG)] IS NULL
-OR
-    [Documented Nutritional Status] IS NULL
-OR
-    [Documented and Calculated Nutritional Status Match] = 'No'
-)
+    AND
+        [Last Visit in Previous Week] = 'Yes'
+    AND
+    (
+        [Current Weight(KG)] IS NULL
+        OR [Current Height(CM)] IS NULL
+        OR [Documented Nutritional Status] IS NULL
+        OR [Documented and Calculated Nutritional Status Match] = 'No'
+    )
