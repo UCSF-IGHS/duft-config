8c1e970a0d72bb35cacd2f9edcc8310e5ef91359
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-06-12 10:51:09 +0300

    Chore: Client Profile

--- system/dashboards/sql/plhiv/table/unsuppressed_clients.sql
+++ system/dashboards/sql/plhiv/table/unsuppressed_clients.sql
@@ -14,7 +14,8 @@ SELECT
 FROM
     duft.fact_duft_sentinel_event
 WHERE
-    (
-        [Last VL Is Unsuppressed] = 'Yes'
-        AND [Last Date Initiated EAC] IS NULL
+    [Last VL Is Unsuppressed] = 'Yes'
+    AND (
+        [Last Date Initiated EAC] IS NULL
+        OR [Last Date Initiated EAC] < [Last VL Result Date]
     )
