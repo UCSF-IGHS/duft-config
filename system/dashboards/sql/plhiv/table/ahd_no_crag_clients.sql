24a9255a6f4236335f99d857b7a5a6ce0ca20564
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-06-12 16:25:09 +0300

    Chore: Update initial columns

--- system/dashboards/sql/plhiv/table/ahd_no_crag_clients.sql
+++ system/dashboards/sql/plhiv/table/ahd_no_crag_clients.sql
@@ -8,7 +8,7 @@ SELECT
     [Is Recently Initiated] AS [TX_NEW],
     [WHO Stage 3/4 (Yes/No)],
     [WHO Stage 3/4 With No CD4 Test],
-    [Traced Back After LTFU] AS [Traced Back After LTFU(TX_RTT)],
+    [Traced Back After LTFU] AS [Traced Back After LTFU (TX_RTT)],
     [Has High VL Post EAC] AS [Persistent High VL After EAC],
     [Last CD4 Test Date],
     [Last CD4 Result Count] AS [Last CD4 Test Result],
