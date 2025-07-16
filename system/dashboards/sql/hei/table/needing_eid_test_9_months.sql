6e5a8ce61a37d27e747690223da52b912ca7cc5c
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-07-16 16:34:11 +0300

    Chore: Correct 9+ and 2 months computation

--- system/dashboards/sql/hei/table/needing_eid_test_9_months.sql
+++ system/dashboards/sql/hei/table/needing_eid_test_9_months.sql
@@ -1,13 +1,13 @@
-SELECT
+SELECT DISTINCT
     [Patient ID] AS [Mother Patient ID],
     [Exposed Infant ID],
     [Date of Birth],
     [HEI Current Age in Months],
     [Sex],
     [Last Visit Date],
-    [HEI Eligible for DNA PCR at 9 Months],
-    [HEI Eligible for DNA PCR at 3 Months Post-BF],
-    [HEI Eligible for Antibody Test at 18 Months]
+    [HEI Eligible for 9 Months DNA PCR],
+    [HEI Eligible for 3 Months Post-BF Antibody Test],
+    [HEI Eligible for 18 Months Antibody Test]
 FROM
     duft.fact_duft_hei_sentinel_event
 WHERE
