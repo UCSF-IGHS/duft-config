558c10ce3e034d81cb2ae2cec97539d39f2ebb0c
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-07-16 14:38:07 +0300

    Chore: Correct HEI 9+ months computation

--- system/dashboards/sql/hei/table/needing_eid_test_9_months.sql
+++ system/dashboards/sql/hei/table/needing_eid_test_9_months.sql
@@ -6,18 +6,17 @@ SELECT
     [Sex],
     [Last Visit Date],
     [HEI Eligible for DNA PCR at 9 Months],
-    [DNA PCR at 9 Months Sample Collection Date],
-    [Antibody Test Results at 3 Months Post-BF],
-    [Antibody Test Date at 3 Months Post-BF],
-    [Antibody Test Results at 18 Months],
-    [Antibody Test Date at 18 Months]
+    [HEI Eligible for DNA PCR at 3 Months Post-BF],
+    [HEI Eligible for Antibody Test at 18 Months]
 FROM
     duft.fact_duft_hei_sentinel_event
 WHERE
-    (
-        [HEI Eligible for DNA PCR at 9 Months] = 'Yes'
-        AND [DNA PCR at 9 Months Sample Collection Date] IS NULL
-        AND [Antibody Test Date at 3 Months Post-BF] IS NULL
-    )
-    AND ISNULL([Infant Status], '') NOT IN ('TRN', 'DIE')
-    AND [HEI Current Age in Months] = 9
+    [HEI Current Age in Months] BETWEEN 9 AND 18
+AND
+    [DNA PCR at 9 Months Sample Collection Date] IS NULL
+AND
+    [Antibody Test Date at 3 Months Post-BF] IS NULL
+AND
+    [Antibody Test Date at 18 Months] IS NULL
+AND
+    ISNULL([Infant Status], '') NOT IN ('TRN', 'DIE')
--- system/dashboards/sql/hei/tile/needing_eid_test_9_months.sql
+++ system/dashboards/sql/hei/tile/needing_eid_test_9_months.sql
@@ -3,10 +3,12 @@ SELECT
 FROM
     duft.fact_duft_hei_sentinel_event
 WHERE
-    (
-        [HEI Eligible for DNA PCR at 9 Months] = 'Yes'
-        AND [DNA PCR at 9 Months Sample Collection Date] IS NULL
-        AND [Antibody Test Date at 3 Months Post-BF] IS NULL
-    )
-    AND ISNULL([Infant Status], '') NOT IN ('TRN', 'DIE')
-    AND [HEI Current Age in Months] = 9
+    [HEI Current Age in Months] BETWEEN 9 AND 18
+AND
+    [DNA PCR at 9 Months Sample Collection Date] IS NULL
+AND
+    [Antibody Test Date at 3 Months Post-BF] IS NULL
+AND
+    [Antibody Test Date at 18 Months] IS NULL
+AND
+    ISNULL([Infant Status], '') NOT IN ('TRN', 'DIE')
