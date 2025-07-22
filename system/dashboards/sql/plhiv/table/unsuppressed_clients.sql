ee484c4115d4c6a954596d9e287b6b0c85f7d628
bernard-siele <bernard.siele@ucglobalprograms.org>
2025-07-22 10:28:57 +0300

    Chore: Update unsuppressed list to select distinct

--- system/dashboards/sql/plhiv/table/unsuppressed_clients.sql
+++ system/dashboards/sql/plhiv/table/unsuppressed_clients.sql
@@ -1,4 +1,23 @@
-SELECT
-    *
+SELECT DISTINCT
+    [Patient ID],
+    [Current Age],
+    [Sex],
+    [Date Start ART],
+    [Last Visit Date],
+    [Last Appointment Date_],
+    [VL Result Return Date],
+    [Last VL Test Date],
+    [Last VL Result Numeric],
+    [ARV Regimen],
+    [Now Pregnant/Breastfeeding],
+    [Previous Date Initiated EAC],
+    [Last Visit Type],
+    [Last Visit Refill Type],
+    [Days Missed Appointment],
+    [ARV Regimen Description],
+    [Current Height (CM)],
+    [Current Weight (KG)],
+    [BP Reading (Systolic)],
+    [BP Reading (Diastolic)]
 FROM
     duft.fact_unsuppressed_no_eac_clients
--- system/dashboards/sql/plhiv/tile/unsuppressed.sql
+++ system/dashboards/sql/plhiv/tile/unsuppressed.sql
@@ -1,4 +1,4 @@
 SELECT
-    COUNT(*)
+    COUNT(DISTINCT [Patient ID])
 FROM
     duft.fact_unsuppressed_no_eac_clients
