1a4e8c636519bbdf314cf2187f4a258a6cdd72d2
Arnold Mosha <arnoldmosha60@gmail.com>
2025-07-25 14:02:20 +0300

    update the rejection reasons query on the sliding dashboard

--- system/dashboards/sql/sliding_dashboard_queries_for_last_week/hvl_reasons_for_sample_rejection.sql
+++ system/dashboards/sql/sliding_dashboard_queries_for_last_week/hvl_reasons_for_sample_rejection.sql
@@ -1,17 +1,15 @@
 SELECT
 	ds.clean_rejection_reason AS category,
 	dd.weekly_start_monday_period AS week_name,
-	COUNT(*) AS value
+	sum(fs.is_rejected_at_the_testing_lab_on_report_date) AS value
 FROM
-	[derived].dim_sample ds
-LEFT JOIN [derived].dim_date dd ON
-	dd.date = ds.lab_received_date
+	[derived].fact_daily_hvl_sample_status fs
+INNER JOIN derived.dim_sample ds on ds.sample_id = fs.sample_id
+INNER JOIN derived.dim_date dd on ds.lab_received_date = dd.[date] 
 WHERE
-	dd.[date] >= DATEADD(DAY, -(DATEPART(WEEKDAY, GETDATE()) + 6), GETDATE())
-	AND dd.[date] <= DATEADD(DAY, 1 - DATEPART(WEEKDAY, GETDATE()), GETDATE())
-	AND ds.test_name = 'HIVVL'
-	AND ds.sample_quality_status = 'RejectedLab'
-	AND ds.clean_rejection_reason <> ''
+(dd.[date] >= DATEADD(DAY, -(DATEPART(WEEKDAY, GETDATE()) + 6), GETDATE())
+	AND dd.[date] <= DATEADD(DAY, 1 - DATEPART(WEEKDAY, GETDATE()), GETDATE()))
+AND fs.is_rejected_at_the_testing_lab_on_report_date = 1
 GROUP BY
 	dd.weekly_start_monday_period,
 	ds.clean_rejection_reason
