267c44d639715c64938e36feaac53303e376dcdf
focushao <focus.medard@gmail.com>
2025-10-06 12:06:47 +0300

    Revert "detailed rejection table"
    This reverts commit de35d3cb0979ff6d0986fd3dbce1d1ce110ecc96.
    
    wait for current PR to be merged before pushing

--- system/dashboards/sql/rejection_reports_dashboard_queries/eid_rejection_detailed_report.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-		sample_tracking_id, [_hfr_id] ,df.facility_name, df.region, df.council,
-		fst.collected_date, fst.lab_received_date, fst.rejection_reason 
-FROM [derived].fact_sample_testing fst 
-INNER JOIN [derived].dim_facility df on df.facility_id = fst.facility_id
-WHERE is_sample_rejected = 1
-    AND is_eid_sample = 1
-    AND lab_received_date >= $[start_date%d]
-    AND lab_received_date <= $[end_date%d]
