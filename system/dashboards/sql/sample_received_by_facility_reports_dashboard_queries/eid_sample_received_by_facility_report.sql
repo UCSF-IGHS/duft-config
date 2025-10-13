93e74124fe89f3ce630e60d138206b0aee483e0d
GoodluckMlungusye <goodluckmlungusye@gmail.com>
2025-10-13 15:14:48 +0300

    Create a report showing samples received by facility

---
+++ system/dashboards/sql/sample_received_by_facility_reports_dashboard_queries/eid_sample_received_by_facility_report.sql
@@ -0,0 +1,21 @@
+SELECT
+    TRIM(f.region) AS Region,
+    TRIM(f.council) AS Council,
+    TRIM(f.facility_name) AS Facility,
+    SUM(s.eid_sample_dbs_received) AS [Samples Received]
+FROM
+    [final].fact_daily_sample_summary s
+    INNER JOIN [derived].dim_facility f ON s.hfr_id_for_HUB_sample_is_coming_from = f.hfr_code
+    INNER JOIN [derived].fact_sample_testing fst ON s.hfr_id_for_HUB_sample_is_coming_from = fst.[_hfr_id] 
+WHERE
+	fst.lab_received_date >= $[start_date%d]
+    AND fst.lab_received_date <= $[end_date%d]
+    AND fst.is_eid_sample = 1
+	AND fst.is_valid_record  = 1
+	AND fst.is_received  = 1
+GROUP BY
+    TRIM(f.region),
+    TRIM(f.council),
+    TRIM(f.facility_name)
+ORDER BY
+    Facility ASC;
