0f44c97340baf24dc53e9a35e320eb36891ec7f5
moovason <moovason@gmail.com>
2026-01-19 15:16:06 +0300

    Removed clients who are marked transferred out from missed appointment

--- system/dashboards/sql/plhiv/table/missed_appointment_clients.sql
+++ system/dashboards/sql/plhiv/table/missed_appointment_clients.sql
@@ -22,6 +22,9 @@ FROM
 WHERE
     [Last Appointment Date] <= GETDATE()
 AND
-    [Is Marked Transferred Out] != 'Yes'
+    (
+        [Is Marked Transferred Out] = 'No'
+        OR [Is Marked Transferred Out] IS NULL
+    )
 AND
     DATEDIFF(DAY, [Last Visit Date], [Last Appointment Date]) > 10
